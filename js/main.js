9d14d3bfb3053e733c8f6bfbd68d4f4d8f3430d0
Luis Treviño <97487146+LuisTrevio@users.noreply.github.com>
2024-09-03 17:15:22 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,6 +29,7 @@ function Pop() {
     document.querySelectorAll('.Pop-Exit').forEach((result) => {result.classList.toggle('Pop-out');})
     document.querySelectorAll('.Status-Ani').forEach((result) => {result.classList.toggle('Status-Animated');})
     document.querySelectorAll('.scr-fr').forEach((result) => {result.classList.toggle('scroll-frost');})
+    document.querySelectorAll('.close-up').forEach((result) => {result.classList.toggle('Pop-Close-Up');})
 
     document.querySelectorAll('.Pop-VS-O').forEach((result) => {result.classList.remove('Pop-VS-Out')})
     document.querySelectorAll('.Pop-Git-O').forEach((result) => {result.classList.remove('Pop-Git-Out')})
